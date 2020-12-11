4e3b6913efe796161bf2305aff0a8e9aedab92ec
Jake Choi <JakeCYG@gmail.com>
2020-12-11 15:50:39 -0600

    Define Commands model and API interface

---
+++ dotNetCoreRestAPI/Data/ICommandsRepo.cs
@@ -0,0 +1,16 @@
+﻿using dotNetCoreRestAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreRestAPI.Data
+{
+    /// <summary>
+    /// RestAPI interfaces
+    /// </summary>
+    interface ICommandsRepo
+    {
+        IEnumerable<Commands> GetCommands();
+    }
+}
---
+++ dotNetCoreRestAPI/Models/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetCoreRestAPI.Models
+{
+    public class Commands
+    {
+        public int Id { get; set; }
+        public string ShortCut { get; set; }
+        public string CommandLine { get; set; }
+        public  string Platform { get; set; }
+
+    }
+}
