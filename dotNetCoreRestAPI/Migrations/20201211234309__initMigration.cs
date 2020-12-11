da4b8e0b4686b5f5c38738b5fea2058fad113124
Jake Choi <JakeCYG@gmail.com>
2020-12-11 17:43:57 -0600

    made Commands model attributes requried + create init migration

---
+++ dotNetCoreRestAPI/Migrations/20201211234309__initMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotNetCoreRestAPI.Migrations
+{
+    public partial class _initMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShortCutTo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Command = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Platform = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
--- dotNetCoreRestAPI/Models/Commands.cs
+++ dotNetCoreRestAPI/Models/Commands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,14 @@ namespace dotNetCoreRestAPI.Models
 {
     public class Commands
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string ShortCutTo { get; set; }
+        [Required]
         public string Command { get; set; }
-        public  string Platform { get; set; }
+        [Required]
+        public string Platform { get; set; }
 
     }
 }
