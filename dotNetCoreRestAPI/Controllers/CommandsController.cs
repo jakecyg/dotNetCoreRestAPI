20172b15f4fa2c1315e0a809813e4eb4d629ac15
Jake Choi <JakeCYG@gmail.com>
2020-12-11 16:21:35 -0600

    Implemented Get all and GetById

--- dotNetCoreRestAPI/Controllers/CommandsController.cs
+++ dotNetCoreRestAPI/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dotNetCoreRestAPI.Data;
+using dotNetCoreRestAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +10,33 @@ namespace dotNetCoreRestAPI.Controllers
 {
     //ApiController = provides some out of the box functionalities
     [ApiController]
+    //api/commands 
+    [Route("api/[controller]")]
     public class CommandsController : Controller
     {
-        public IActionResult Index()
+        private readonly MockCommandsRepo _repo = new MockCommandsRepo();
+
+        //public IActionResult Index()
+        //{
+
+        //}
+
+        //respond to HttpGet requests with uri: api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Commands>> GetAllCommands()
         {
-            return View();
+            var commands = _repo.GetCommands();
+            //return 200 success
+            return Ok(commands);
+        }
+
+        //respond to get requests with uri: api/commands/id
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Commands>> GetComandById(int id)
+        {
+            var command = _repo.GetCommandById(id);
+            return Ok(command);
+
         }
     }
 }
